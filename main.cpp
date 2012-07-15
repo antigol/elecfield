86fd2ccb31a0242ea1cdf21efcfa583d832c516d
Mario Geiger <geiger.mario@gmail.com>
2012-07-15 16:37:33 +0200

    GLFormat

--- main.cpp
+++ main.cpp
@@ -7,25 +7,9 @@ int main(int argc, char *argv[])
     qsrand(a.applicationPid());
 
 
-    if (QGLFormat::openGLVersionFlags().testFlag(QGLFormat::OpenGL_Version_3_3)) {
-        qDebug("Run OpengGL version 3.3");
 
-        // Specify an OpenGL 3.3 format using the Core profile.
-        // That is, no old-school fixed pipeline functionality
-        QGLFormat glFormat;
-        glFormat.setVersion(3, 3);
-        glFormat.setProfile(QGLFormat::CoreProfile); // Requires >=Qt-4.8.0
-        glFormat.setSampleBuffers(true);
-        GLWidget w(glFormat);
-        w.show();
-
-        return a.exec();
-    } else {
-        qDebug("Run OpengGL default version");
-
-        GLWidget w(QGLFormat::defaultFormat());
-        w.show();
-
-        return a.exec();
-    }
+    GLWidget w(QGLFormat::defaultFormat());
+    w.show();
+    
+    return a.exec();
 }
